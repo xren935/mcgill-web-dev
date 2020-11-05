db6fdb86d24d3c4f1737ab54597581361562a6f7
xren935 <xingya.ren@mail.mcgill.ca>
2020-11-05 00:44:47 -0500

    assign9 done✅

--- mini-assign9/checkpass.py
+++ mini-assign9/checkpass.py
@@ -1,35 +1,15 @@
 #!/usr/bin/python
-import csv
-import cgi, cgitb
-import sys, json
+import cgi, cgitb, csv, sys
 
-import sys, json
+form=cgi.FieldStorage()
+username=form.getvalue("username")
+password=form.getvalue("password")
 
-result = {'success':'true','message':'The Command Completed Successfully'}
-
-myjson = json.load(sys.stdin)
-# Do something with 'myjson' object
-
-print ('Content-Type: application/json\n\n')
-print (json.dumps(result) )   # or "json.dump(result, sys.stdout)"
-
-
-nameToPassword = dict() 
- 
-
-with open('users.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    
-    for row in csv_reader:
-            # print(f'\t"name" {row[0]}')
-            # print(f'\t"pswrd" {row[1]}')
-            currName = row[0]
-            currPswrd = row[1] 
-            nameToPassword[currName] = currPswrd
-            line_count += 1
-    # print(f'Processed {line_count} lines.')
-
-    # print (nameToPassword['Annie'])
-
- 
+print("ContentType:text/html\n\n")
+with open('users.csv') as csvFile:
+    reader = csv.reader(csvFile)
+    for row in reader:
+        if (row[0] == username and row[1] == password):
+            print("Your Password Matches")
+            sys.exit()
+    print("Wrong username or password")
